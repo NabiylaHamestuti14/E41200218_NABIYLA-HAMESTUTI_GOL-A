1e4708a10d89937fc92d98f0afeaabe794834160
NabiylaHamestuti14 <naufalhisyam266@gmail.com>
2021-03-16 22:06:42 +0700

    Create Variabel koreksi.java

---
+++ Minggu 2/tugas/Variabel koreksi.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication21;
+
+/**
+ *
+ * @author Yuangga Yuda Negara
+ */
+public class JavaApplication21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean gameOver = false; //penulisan banar
+        
+        int student = 50, classes = 3; //penulisan benar
+        
+        double sales_tax; //penulisan benar
+        
+        short number1; //penulisan benar
+        
+        int 2beOrNot2be; //penulisan variabel salah dikarenakan keyword integer hanya berlaku untuk deklarasi angka
+        
+        float price index; //penulisan variabel salah dikarenakan nama pada variabel tidak boleh menggunakan spasi
+        //seharusnya pada price indetx digabung yaitu PriceIndex
+        
+        double lastYear'sPrice; //penulisan variabel salah karena pemberian nama pada variabel tidak boleh menggunakan
+        //tanda petik seharusnya lastYearsPrice
+        
+        long class; //penulisan salah dikarenakan penamaan varibel tidak boleh menggunakan kata class,
+        //karena class merupakan sebuah entitas yang ada pada java
+    }
+    
+}
