7dc96b01a96ccbe2e36d91ed21a9a4ff2c498352
NabiylaHamestuti14 <naufalhisyam266@gmail.com>
2021-03-16 22:06:08 +0700

    Create Latihan2.java

---
+++ Minggu 2/latihan/Latihan2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan2;
+
+/**
+ *
+ * @author Nabiyla Hamestuti
+ */
+public class Latihan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nama = "Nabiyla Hamestuti";
+        char abjad = 'n';
+        int n = 20;
+        double m = 20.5;
+        
+        System.out.println(nama);
+        System.out.println(n);
+    }
+    
+}
