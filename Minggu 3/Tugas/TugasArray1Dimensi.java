56e04df08a7d14dd56e3c6331b949d5a347ff793
NabiylaHamestuti14 <naufalhisyam266@gmail.com>
2021-03-23 19:22:02 +0700

    Create TugasArray1Dimensi.java

---
+++ Minggu 3/Tugas/TugasArray1Dimensi.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugasarray1dimensi;
+
+/**
+ *
+ * @author Nabiyla Hamestuti
+ */
+public class TugasArray1Dimensi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String [] nama = {"Nabiyla", "Hamestuti"};
+        
+        for (int n=0; n<nama.length; n++){
+            System.out.print(nama [n]);
+            System.out.print(" ");
+        }           
+    }    
+}
