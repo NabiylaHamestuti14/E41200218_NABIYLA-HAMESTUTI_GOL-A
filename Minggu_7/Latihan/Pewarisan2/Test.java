dfa07eddcd8b326f2a6c38cc4a6f7379adcba1ba
NabiylaHamestuti14 <naufalhisyam266@gmail.com>
2021-04-25 20:36:52 +0700

    Create Test.java

---
+++ Minggu_7/Latihan/Pewarisan2/Test.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pewarisan2;
+
+/**
+ *
+ * @author Nabiyla Hamestuti
+ */
+public class Test {
+    public static void main(String[] args) {
+        Pelajar Mahasiswa = new Pelajar("Nabiyla", 160, 58, "E41200218", "Politeknik Negeri Jember", 88.8);
+        System.out.println(Mahasiswa.toString());
+    }
+}
